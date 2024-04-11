fd6d84b32490e0e67e3a25dedb0d7b7a39999200
CarlosCumpa <87941522+CarlosCumpa@users.noreply.github.com>
2024-04-10 23:27:43 -0500

    Actualización en los Controladores
    Actualización de los controladores en las entidades Eventos y Tipo Plan, se agregó logback-spring.xml para registrar los errores, se agregó las dependencias de Swagger UI

---
+++ healthycookingb/src/main/java/com/upc/healthycookingb/services/EventoService.java
@@ -0,0 +1,40 @@
+package com.upc.healthycookingb.services;
+
+import com.upc.healthycookingb.entities.Evento;
+import com.upc.healthycookingb.repository.EventosRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventoService {
+
+    @Autowired
+    private EventosRepository eventosRepository;
+
+    public Evento save(Evento eventos){return eventosRepository.save(eventos);}
+
+    public List<Evento> list(){return eventosRepository.findAll();}
+
+
+    public Evento update(Evento eventos) throws Exception{ //se propaga la excepcion
+        eventosRepository.findById(eventos.getId()).orElseThrow(()-> new Exception("No se actualizó"));
+        return eventosRepository.save(eventos);//actualiza si existe el author
+    }
+    
+    public Evento delete(Integer id) throws Exception {
+        Evento eventos;
+        eventos = eventosRepository.findById(id).orElseThrow(()-> new Exception("No existe evento"));
+        eventosRepository.delete(eventos);
+        return eventos;
+    }
+    public List<Evento> listFirstEvent(String prefijo){
+        return eventosRepository.findEventoByNombreeventoStartingWith(prefijo);
+    }
+    public Evento search(Integer id) throws Exception {
+        return eventosRepository.findById(id).orElseThrow(()->new Exception("No existe"));
+    }
+
+}
