5ca5dd7965df371678b1db1fabe40c0cf342da54
CarlosCumpa <87941522+CarlosCumpa@users.noreply.github.com>
2024-05-02 12:54:32 -0500

    Delete healthycookingd directory

--- healthycookingd/src/main/java/com/upc/healthycookingd/repository/PerfilusuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.healthycookingd.repository;
-
-import com.upc.healthycookingd.entities.Perfilusuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilusuarioRepository extends JpaRepository<Perfilusuario, Integer> {
-}
